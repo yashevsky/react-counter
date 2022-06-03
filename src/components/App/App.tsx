1f7287256b1755502e5f70b27ffe6af195af53e0
Ilya Tyurin <il08082000@gmail.com>
2022-06-03 14:30:41 +0300

    кнопки и useEffect

---
+++ src/components/App/App.tsx
@@ -0,0 +1,56 @@
+import React, {useEffect, useState} from 'react'
+import Button from '../Button'
+import styles from './styles.module.css'
+
+const App = () => {
+    const [counter, setCounter] = useState(0)
+    const [secondCounter, setSecondCounter] = useState(0)
+
+    const increment = () => setCounter(counter + 1)
+    const decrement = () => setCounter(counter - 1)
+    const resetCounter = () => setCounter(0)
+    const multiply10 = () => setCounter(counter * 10)
+    const division10 = () => setCounter(counter / 10)
+
+    const newCounterInc = () => setSecondCounter(secondCounter + 1)
+    const func = () => console.log('нажатие на window')
+
+
+    useEffect(() => {
+        // ComponentDidMount и ComponentDidUpdate и ComponentWillUpdate
+        console.log('что-то из списка зависимостей изменилось')
+
+        window.addEventListener('click', func)
+
+        // ComponentWillUnmount
+        return () => {
+            window.removeEventListener('click', func)
+            console.log('убрали листенер')
+        }
+    }, [counter])
+
+
+  return (
+      // перенести стили в файл ксс
+      <div style={{display: 'flex', minHeight: '100vh', alignItems: 'center', justifyContent: 'center', flexDirection:'column'}}>
+          {counter < 0 ? (<h1>Значение меньше 0</h1>) : (<h1>{counter}</h1>)}
+          <div>
+              <Button className={styles.firstButton} color="green" onClick={multiply10}>*10</Button>
+              <Button color="salmon" onClick={increment}>+</Button>
+              <Button color="yellow" onClick={resetCounter}>0</Button>
+              <Button color="red" onClick={decrement}>-</Button>
+              <Button color="green" onClick={division10}>/10</Button>
+          </div>
+          <div>
+              {secondCounter}
+              <Button onClick={newCounterInc}>+1</Button>
+          </div>
+      </div>
+  )
+}
+
+export default App
+
+
+// доп функц, репо, добавить AsheR4444
+
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,31 @@
 import React, { FC } from 'react'
+import classnames from 'classnames'
 
 import styles from './style.module.css'
 
-type ButtonType = {
+type ButtonProps = {
     children: React.ReactNode,
-    onClick: () => void
+    onClick: () => void,
+    color?: 'salmon' | 'yellow' | 'green' | 'red',
+    className?: string | undefined
 }
 
-const Button: FC<ButtonType> = ({children, onClick}) => {
+const Button: FC<ButtonProps> = ({
+    children,
+    onClick,
+    color,
+    className
+}) => {
+    const classNames = classnames(
+        styles.root,
+        className,
+        {[styles.salmon]: color === 'salmon'},
+        {[styles.red]: color === 'red'},
+        {[styles.yellow]: color === 'yellow'},
+        {[styles.green]: color === 'green'},
+    )
   return (
-    <button onClick={onClick} className={styles.root}>{children}</button>
+    <button onClick={onClick} className={classNames}>{children}</button>
   )
 }
 
